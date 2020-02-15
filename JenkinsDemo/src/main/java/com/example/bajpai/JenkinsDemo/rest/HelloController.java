f5cc53208b51b06edd18cb131b9d8e86749c44b1
Gaurav Bajpai <penguin@Gauravs-MacBook-Air.local>
2020-02-15 06:45:04 +0530

    Adding Spring Hello World

---
+++ JenkinsDemo/src/main/java/com/example/bajpai/JenkinsDemo/rest/HelloController.java
@@ -0,0 +1,14 @@
+package com.example.bajpai.JenkinsDemo.rest;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+	@RequestMapping("/")
+	public String index() {
+		return "Greetings from Spring Boot!";
+	}
+
+}
